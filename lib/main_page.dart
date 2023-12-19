020d2cc270ea0cd266a628657a3714e7c4c9bb63
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-19 17:21:05 +0900

    037. 로그인 버튼 누르면 화면갱신을 통해서 글자표시 하기

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -102,13 +102,17 @@ class _MainPageState extends State<MainPage> {
                     child: ElevatedButton(
                       onPressed: () {
                         print(_textController.text);
-                          print(_text);
+                          // 화면 갱신
+                          setState(() {
+
+                          });
                       },
                       child: Text('로그인'),
                     ),
                   ),
                 ],
               ),
+          Text(_textController.text),
               //이미지 위젯 네트워크 이미지복사 와 다운로드 해서 assets 파일 넣고 yaml 안에 사진 경로넣기
               // 이미지 채우기
               Image.network(
