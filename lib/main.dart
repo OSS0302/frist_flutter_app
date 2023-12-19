7d068e9d85374843b5d14ace6a7ff6082ac9ad7a
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-19 15:20:17 +0900

    021.  하나의 메인 파일 에서 마이 홈 페이지 로 파일 나누기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frist_flutter_app/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,77 +22,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('홈 화면 '),
-      ),
-      body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.end, // 메인방향 정렬 ,
-          mainAxisAlignment: MainAxisAlignment.center, // 메인방향 정렬 ,
-          children: [
-            Text(
-              '숫자',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '${count}',
-              style: TextStyle(
-                fontSize: 60,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('ElevatedButton');
-              },
-              child: Text(
-                'ElevatedButton',
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                print('TextButton');
-              },
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print('OutlinedButton');
-              },
-              child: Text('TextButton'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 화면 갱신
-          setState(() {
-            count++;
-          });
-        },
-        child: Icon(
-          Icons.add,
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('홈 화면 '),
+      ),
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.end, // 메인방향 정렬 ,
+          mainAxisAlignment: MainAxisAlignment.center, // 메인방향 정렬 ,
+          children: [
+            Text(
+              '숫자',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${count}',
+              style: TextStyle(
+                fontSize: 60,
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('ElevatedButton');
+              },
+              child: Text(
+                'ElevatedButton',
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                print('TextButton');
+              },
+              child: Text('TextButton'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                print('OutlinedButton');
+              },
+              child: Text('TextButton'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // 화면 갱신
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
