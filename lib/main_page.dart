af0b9a6d71616d29be98995343eccf1d1437c3d6
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-19 16:53:51 +0900

    033. 공사중 해결 SingleChildScrollView

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -19,90 +19,93 @@ class _MainPageState extends State<MainPage> {
         title: Text('홈 화면 '),
       ),
       body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.end, // 메인방향 정렬 ,
-          mainAxisAlignment: MainAxisAlignment.center, // 메인방향 정렬 ,
-          children: [
-            // Container 영역
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            //둘다 비슷하다 색깔 주는거 모양주는 말곤 같다.
-            SizedBox(height: 30),
-            Container(height: 10),
-            Text(
-              '숫자',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '${number}',
-              style: TextStyle(
-                fontSize: 60,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.end, // 메인방향 정렬 ,
+            mainAxisAlignment: MainAxisAlignment.center, // 메인방향 정렬 ,
+            children: [
+              // Container 영역
+              Container(
                 color: Colors.red,
-                fontWeight: FontWeight.bold,
+                width: 100,
+                height: 100,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('ElevatedButton');
-              },
-              child: Text(
-                'ElevatedButton',
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                print('TextButton');
-              },
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print('OutlinedButton');
-              },
-              child: Text('TextButton'),
-            ),
-            // 텍스트 필드 위젯 os 상태에서 영어는 잘되지만 한글 잘 안된다.
-            TextField(
-              decoration: InputDecoration(
-                labelText: '글자 입력',
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (text) {
-                print(text);
-              },
-            ),
-            //이미지 위젯 네트워크 이미지복사 와 다운로드 해서 assets 파일 넣고 yaml 안에 사진 경로넣기
-            // 이미지 채우기
-            Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZhJSwfqNWUuuNpqppZqdAx8v7OlFle5Csr6NNf9SgHG8zuihCnbzHO9yQmviv7eXe34&usqp=CAU',
-              width: 100,
-              height: 100,
-              // 박스 꽉 채우기
-              fit: BoxFit.cover,
-            ),
-            Container(
-              // 크기:(컨테이너>이미지) 컨테이너 크기 넓이 120 높이 120 이면 이미지 크기가  컨테이너 크기를 따라간다.
-              // 크기:(컨테이너<이미지) 자식인 이미지 가 컨테이너 보다 크면 이미지 크기를 따라간다.
-              color: Colors.red,
-              // 패딩 주기
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  'assets/pic.jpg',
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
+              //둘다 비슷하다 색깔 주는거 모양주는 말곤 같다.
+              SizedBox(height: 30),
+              Container(height: 10),
+              Text(
+                '숫자',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ],
+              Text(
+                '${number}',
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('ElevatedButton');
+                },
+                child: Text(
+                  'ElevatedButton',
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  print('TextButton');
+                },
+                child: Text('TextButton'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  print('OutlinedButton');
+                },
+                child: Text('TextButton'),
+              ),
+              // 텍스트 필드 위젯 os 상태에서 영어는 잘되지만 한글 잘 안된다.
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '글자 입력',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  print(text);
+                },
+              ),
+              //이미지 위젯 네트워크 이미지복사 와 다운로드 해서 assets 파일 넣고 yaml 안에 사진 경로넣기
+              // 이미지 채우기
+              Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZhJSwfqNWUuuNpqppZqdAx8v7OlFle5Csr6NNf9SgHG8zuihCnbzHO9yQmviv7eXe34&usqp=CAU',
+                width: 100,
+                height: 100,
+                // 박스 꽉 채우기
+                fit: BoxFit.cover,
+              ),
+              Container(
+                // 크기:(컨테이너>이미지) 컨테이너 크기 넓이 120 높이 120 이면 이미지 크기가  컨테이너 크기를 따라간다.
+                // 크기:(컨테이너<이미지) 자식인 이미지 가 컨테이너 보다 크면 이미지 크기를 따라간다.
+                color: Colors.red,
+                // 패딩 주기
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/pic.jpg',
+                    width: 100,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
